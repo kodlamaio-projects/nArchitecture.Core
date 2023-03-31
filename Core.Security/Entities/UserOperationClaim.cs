c97518a2e6c805ee4f3e7ca8f783378e8c68a336
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2023-03-31 18:22:45 +0300

    refactor(persistence): add generic id type to base entity

--- Core.Security/Entities/UserOperationClaim.cs
+++ Core.Security/Entities/UserOperationClaim.cs
@@ -2,7 +2,7 @@
 
 namespace Core.Security.Entities;
 
-public class UserOperationClaim : Entity
+public class UserOperationClaim : Entity<int>
 {
     public int UserId { get; set; }
     public int OperationClaimId { get; set; }
