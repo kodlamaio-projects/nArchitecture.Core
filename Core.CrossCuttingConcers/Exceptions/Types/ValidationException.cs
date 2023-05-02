8982982ccd698a64e9f412ffe7c357f4d695a678
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2023-05-02 18:23:44 +0300

    fix(crossCuttingConcers): resolve CS8604

--- Core.CrossCuttingConcers/Exceptions/Types/ValidationException.cs
+++ Core.CrossCuttingConcers/Exceptions/Types/ValidationException.cs
@@ -13,7 +13,7 @@ public class ValidationException : Exception
     private static string BuildErrorMessage(IEnumerable<ValidationExceptionModel> errors)
     {
         IEnumerable<string> arr = errors.Select(
-            x => $"{Environment.NewLine} -- {x.Property}: {string.Join(Environment.NewLine, x.Errors)}"
+            x => $"{Environment.NewLine} -- {x.Property}: {string.Join(Environment.NewLine, values: x.Errors ?? Array.Empty<string>())}"
         );
         return $"Validation failed: {string.Join(string.Empty, arr)}";
     }
