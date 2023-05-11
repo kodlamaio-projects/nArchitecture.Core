66447925959d50193efc227fa88d982263be2477
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2023-05-11 15:01:22 +0300

    feat(Core.WebAPI): add swagger bearer security requirement operation filter

---
+++ Core.WebAPI/Extensions/Swagger/BearerSecurityRequirementOperationFilter.cs
@@ -0,0 +1,30 @@
+﻿using Core.Application.Pipelines.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Core.WebAPI.Extensions.Swagger;
+
+public class BearerSecurityRequirementOperationFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        if (!context.MethodInfo.GetParameters().Any(x => x.ParameterType.GetInterfaces().Contains(typeof(ISecuredRequest))))
+            return;
+
+        OpenApiSecurityRequirement authAttribute =
+            new()
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header,
+                    },
+                    Array.Empty<string>()
+                }
+            };
+        operation.Security.Add(authAttribute);
+    }
+}
